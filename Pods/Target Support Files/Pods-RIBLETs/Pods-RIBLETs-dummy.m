a6bc0d66dfb09a8952b79978b09e88ceabe7dfe2
Abhishek Agarwal <abhishek.agarwal@grabtaxi.com>
2019-06-19 13:19:09 +0800

    Added .gitignore

--- Pods/Target Support Files/Pods-RIBLETs/Pods-RIBLETs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RIBLETs : NSObject
-@end
-@implementation PodsDummy_Pods_RIBLETs
-@end
--- Pods/Target Support Files/Pods-RIBLETsTests/Pods-RIBLETsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RIBLETsTests : NSObject
-@end
-@implementation PodsDummy_Pods_RIBLETsTests
-@end
